e8290289092fc11d542045cc0a445bb97c8dd547
Glitch (exercise-trackergm) <none>
2018-09-08 06:15:55 +0000

    🐆🗾 Updated with Glitch

---
+++ server.js
@@ -0,0 +1,43 @@
+const express= require('express');
+var app= express();
+const bodyParser= require('body-parser');
+const mongoose= require('mongoose');
+mongoose.connect(process.env.MLAB_URI || 'mongodb://localhost/exercise-track');
+const cors= require('cors');
+app.use(cors());
+app.use(bodyParser.urlencoded({extended:false}))
+app.use(bodyParser.json());
+
+
+app.use(express.static('public'));
+app.get('/', function(req, res){
+res.sendFile(__dirname + '/views/index.html');
+});
+
+//Not found midleware
+app.use(function(req, res, next){
+     return next({status: 404, message:'not found'})  
+});
+
+// Error handling midleware
+app.use((err, req, res, next) => {
+  let errCode, errMessage
+
+  if (err.errors) {
+    // mongoose validation error
+    errCode = 400 // bad request
+    const keys = Object.keys(err.errors)
+    // report the first validation error
+    errMessage = err.errors[keys[0]].message
+  } else {
+    // generic or custom error
+    errCode = err.status || 500
+    errMessage = err.message || 'Internal Server Error'
+  }
+  res.status(errCode).type('txt')
+    .send(errMessage)
+})
+
+const listener = app.listen(process.env.PORT || 3000, () => {
+  console.log('Your app is listening on port ' + listener.address().port)
+})
